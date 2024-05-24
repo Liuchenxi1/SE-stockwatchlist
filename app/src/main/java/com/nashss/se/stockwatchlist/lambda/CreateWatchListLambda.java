1a110f5e02cc46852c41bcb10a9a242e8b8bfbc9
Liuchenxi1 <87050797+Liuchenxi1@users.noreply.github.com>
2024-05-24 06:51:59 -0500

    Update CreateWatchListLambda.java

--- app/src/main/java/com/nashss/se/stockwatchlist/lambda/CreateWatchListLambda.java
+++ app/src/main/java/com/nashss/se/stockwatchlist/lambda/CreateWatchListLambda.java
@@ -1,15 +1,15 @@
-// package com.nashss.se.stockwatchlist.lambda;
+package com.nashss.se.stockwatchlist.lambda;
 
-// import com.amazonaws.services.lambda.runtime.RequestHandler;
-// import com.nashss.se.stockwatchlist.activity.requests.CreateWatchListRequest;
-// import com.nashss.se.stockwatchlist.activity.results.CreateWatchListResult;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.nashss.se.stockwatchlist.activity.requests.CreateWatchListRequest;
+import com.nashss.se.stockwatchlist.activity.results.CreateWatchListResult;
 
-// public class CreateWatchListLambda
-//         extends LambdaActivityRunner<CreateWatchListRequest, CreateWatchListResult>
-//         implements RequestHandler<AuthenticatedLambdaRequest<CreateWatchListRequest>, LambdaResponse> {
+public class CreateWatchListLambda
+        extends LambdaActivityRunner<CreateWatchListRequest, CreateWatchListResult>
+        implements RequestHandler<AuthenticatedLambdaRequest<CreateWatchListRequest>, LambdaResponse> {
 
-//     @Override
-//     public LambdaResponse handleRequest (AuthenticatedLambdaRequest<Create>)
+    @Override
+    public LambdaResponse handleRequest (AuthenticatedLambdaRequest<Create>)
 
 
-// }
+}
