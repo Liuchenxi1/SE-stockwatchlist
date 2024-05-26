4094696ee7535b19f8054ba9b78462ff70bd6b26
Liu chenxi <shinnkiryu@gmail.com>
2024-05-26 12:56:34 -0500

    000005/////26

--- app/src/main/java/com/nashss/se/stockwatchlist/activity/SearchStockInfoActivity.java
+++ app/src/main/java/com/nashss/se/stockwatchlist/activity/SearchStockInfoActivity.java
@@ -1,4 +1,31 @@
 package com.nashss.se.stockwatchlist.activity;
 
+import com.nashss.se.stockwatchlist.activity.results.SearchStockInfoResult;
+import com.nashss.se.stockwatchlist.models.StockInfo;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.inject.Inject;
+
 public class SearchStockInfoActivity {
+    private final Logger logger = LogManager.getLogger();
+    private final StockInfo stockInfo;
+
+    //No neeed WatchListDao;
+
+    @Inject
+    public SearchStockInfoActivity(StockInfo stockInfo) {
+        this.stockInfo = stockInfo;
+    }
+
+    public static Builder builder() { return new Builder(); }
+
+    public static class Builder {
+
+
+
+
+
+    }
+
 }
