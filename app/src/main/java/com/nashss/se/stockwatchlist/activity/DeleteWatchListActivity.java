3088c95313ab015990009798c579c921c90426de
Liu chenxi <shinnkiryu@gmail.com>
2024-05-30 15:00:09 -0500

    05/30

--- app/src/main/java/com/nashss/se/stockwatchlist/activity/DeleteWatchListActivity.java
+++ app/src/main/java/com/nashss/se/stockwatchlist/activity/DeleteWatchListActivity.java
@@ -1,5 +1,25 @@
 package com.nashss.se.stockwatchlist.activity;
 
+
+import com.nashss.se.stockwatchlist.dynamodb.WatchListDao;
+import com.nashss.se.stockwatchlist.metrics.MetricsPublisher;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.inject.Inject;
+
 public class DeleteWatchListActivity {
+
+    private final Logger log = LogManager.getLogger();
+    private final WatchListDao watchListDao;
+
+    private final MetricsPublisher metricsPublisher;
+
+    @Inject
+
+
+
+
+
 
 }
