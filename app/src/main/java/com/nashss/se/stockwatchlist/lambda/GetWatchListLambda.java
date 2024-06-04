8a611090dd643c53a4ea2159b5bb8f9d48e98a59
Liu chenxi <shinnkiryu@gmail.com>
2024-06-04 09:15:08 -0500

    get watchlist is done

--- app/src/main/java/com/nashss/se/stockwatchlist/lambda/GetWatchListLambda.java
+++ app/src/main/java/com/nashss/se/stockwatchlist/lambda/GetWatchListLambda.java
@@ -9,21 +9,25 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class GetWatchListLambda extends LambdaActivityRunner<GetWatchListRequest, GetWatchListResult>
-        implements RequestHandler<LambdaRequest<GetWatchListRequest>, LambdaResponse> {
+        implements RequestHandler<AuthenticatedLambdaRequest<GetWatchListRequest>, LambdaResponse> {
 
     private final Logger log = LogManager.getLogger();
 
     @Override
-    public LambdaResponse handleRequest(LambdaRequest<GetWatchListRequest> input, Context context) {
-        log.info("handleRequest");
+    public LambdaResponse handleRequest(AuthenticatedLambdaRequest<GetWatchListRequest> input, Context context) {
+        return super.runActivity(() -> {
+                    GetWatchListRequest unauthenticatedRequest = input.fromBody(GetWatchListRequest.class);
 
-        return super.runActivity(() -> input.fromPath(path ->
-                GetWatchListRequest.builder()
-                        .withWatchlistName(path.get("watchlistName"))
-                        .build()),
+                    log.info("Deleting watchlist: {}", unauthenticatedRequest.getWatchlistName());
+
+                    return input.fromUserClaims(claims ->
+                            GetWatchListRequest.builder()
+                                    .withEmail(claims.get("email"))
+                                    .withWatchlistName(unauthenticatedRequest.getWatchlistName())
+                                    .build());
+                },
                 (request, serviceComponent) ->
                         serviceComponent.provideGetWatchListActivity().handleRequest(request)
         );
-
     }
 }
