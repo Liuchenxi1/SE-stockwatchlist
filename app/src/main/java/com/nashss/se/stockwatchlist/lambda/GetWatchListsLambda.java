53dc329f4660d62e6f5104d27287ba34a6a3b103
Liu chenxi <shinnkiryu@gmail.com>
2024-06-12 12:17:03 -0500

    getting the watchlists now

--- app/src/main/java/com/nashss/se/stockwatchlist/lambda/GetWatchListsLambda.java
+++ app/src/main/java/com/nashss/se/stockwatchlist/lambda/GetWatchListsLambda.java
@@ -7,23 +7,25 @@ import com.nashss.se.stockwatchlist.activity.results.GetWatchListsResult;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-//public class GetWatchListsLambda   extends LambdaActivityRunner<GetWatchListsRequest, GetWatchListsResult>
-//        implements RequestHandler<LambdaRequest<GetWatchListsRequest>, LambdaResponse> {
-//
-//    private final Logger log = LogManager.getLogger();
-//
-//    @Override
-//    public LambdaResponse handleRequest((AuthenticatedLambdaRequest<GetWatchListsRequest> input, Context context) {
-//        log.info("handleRequest received with input: {}", input);
-//
-//        return super.runActivity(
-//                () -> input.fromPath(path ->
-//                    GetWatchListsRequest.builder()
-//                            .withEmail(path.get("email"))
-//                            .build()
-//                ),
-//                (request, serviceComponent) ->
-//                        serviceComponent.provideGetWatchListsActivity().handleRequest(request)
-//        );
-//    }
-//}
+public class GetWatchListsLambda extends LambdaActivityRunner<GetWatchListsRequest, GetWatchListsResult>
+        implements RequestHandler<AuthenticatedLambdaRequest<GetWatchListsRequest>, LambdaResponse> {
+
+    private final Logger log = LogManager.getLogger();
+
+    @Override
+    public LambdaResponse handleRequest(AuthenticatedLambdaRequest<GetWatchListsRequest> input, Context context) {
+        return super.runActivity(() -> {
+            GetWatchListsRequest unauthenticatedRequest = input.fromBody(GetWatchListsRequest.class);
+
+            log.info("getting watchlist: {}", unauthenticatedRequest.getEmail());
+
+            return input.fromUserClaims(claims ->
+                    GetWatchListsRequest.builder()
+                            .withEmail(claims.get("email"))
+                            .build());
+        },
+                (request, serviceComponent) ->
+                        serviceComponent.provideGetWatchListsActivity().handleRequest(request)
+                );
+    }
+}
